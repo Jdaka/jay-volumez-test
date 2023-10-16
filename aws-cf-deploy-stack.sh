dcc7e5b6e00bf0d77e268c235c78846961e69dc6
Jdaka <Jay.Daka@gmail.com>
2023-10-16 13:31:31 +0300

    Update aws-cf-deploy-stack.sh

--- aws-cf-deploy-stack.sh
+++ aws-cf-deploy-stack.sh
@@ -8,6 +8,7 @@ fi
 
 # Parameters
 TEMPLATE_URL="https://raw.githubusercontent.com/Jdaka/jay-volumez-test/main/cross-account-role.yaml"
+LOCAL_TEMPLATE_FILE="cross-account-role.yaml"
 STACK_NAME="Volumez-Create-Role-Stack"
 
 # Check if REGION is passed as an argument, if not, prompt the user
@@ -17,9 +18,13 @@ else
     REGION=$1
 fi
 
-# Deploy the CloudFormation template
+# Download the CloudFormation template
+echo "Downloading CloudFormation template..."
+curl -O $TEMPLATE_URL
+
+# Deploy the CloudFormation template using the local file
 echo "Deploying Volumez CloudFormation stack..."
-aws cloudformation create-stack --stack-name $STACK_NAME --template-url $TEMPLATE_URL --capabilities CAPABILITY_NAMED_IAM --region $REGION
+aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://$LOCAL_TEMPLATE_FILE --capabilities CAPABILITY_NAMED_IAM --region $REGION
 
 # Wait for the stack to be created
 echo "Waiting for stack to be created..."
